6c278f881b53ff4987d50c5a6cb8ced74cb06b8c
augus <august.soeltoft@gmail.com>
2026-05-05 10:11:43 +0200

    Initial commit

---
+++ src/main/java/com/HolgersDream/Deckforge/DeckforgeApplication.java
@@ -0,0 +1,13 @@
+package com.HolgersDream.Deckforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HolgersDream/Deckforge/DeckforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HolgersDream.Deckforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeckforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
