d9852f55e7009b7db84493a0ff1c997bf15f3910
augus <august.soeltoft@gmail.com>
2026-05-07 13:43:45 +0200

    Uddybede exceptionhandling en smule og lavede en lille ændring til register

--- src/main/java/com/HolgersDream/Deckforge/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/HolgersDream/Deckforge/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,6 +13,13 @@ public class GlobalExceptionHandler {
     public String handleDatabaseConnection(DatabaseConnectionException dce, Model model){
         model.addAttribute("type", "Database fejl");
         model.addAttribute("errorMessage", dce.getMessage());
+        return "error";
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public String handleDataAccess(DataAccessException dae, Model model){
+        model.addAttribute("type", "Data-adgangs fejl");
+        model.addAttribute("errorMessage", dae.getMessage());
         return "error";
     }
 
@@ -33,6 +41,23 @@ public class GlobalExceptionHandler {
     public String handleLoginValidation(LoginValidationException lve, RedirectAttributes redirectAttributes){
         redirectAttributes.addFlashAttribute("responseMessage", lve.getMessage());
         return "redirect:/authentication/login";
+    }
+
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public String handleNoResourceFound(NoResourceFoundException nrfe, Model model){
+        model.addAttribute("type", "Ukendt side");
+        model.addAttribute("errorMessage", "Siden eller resourcen du prøvede at få fat på virker ikke til at eksistere");
+        return "error";
+    }
+
+
+
+    @ExceptionHandler(Exception.class)
+    public String handleGeneric(Exception ex, Model model){
+        model.addAttribute("type", "Ukendt fejl");
+        model.addAttribute("errorMessage", "Det er ukendt hvad der gik galt");
+        return "error";
     }
 
 
