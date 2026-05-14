b6b33aec5f6e17f25d8bed08f39195bd0bb065c2
josef <josefinebechfrandsen@gmail.com>
2026-05-14 12:27:52 +0200

    Har oprette data.sql og schema.sql og lavet et test kort.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO card_list (neutral_mana, white_mana, name, card_type, picture, set_name, rule_text, rarity)
+VALUES (4, 2, 'The Cheese Stands Alone', 'ENCHANTMENT', 'the-cheese-stands-alone.png', 'Unglued',
+        'When you control no permanents other than this enchantment and have no cards in hand, you win the game.', 'RARE');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,87 @@
+DROP TABLE IF EXISTS user_participate_event;
+DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS owned_card;
+DROP TABLE IF EXISTS deck_contain_card;
+DROP TABLE IF EXISTS deck;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS card_list;
+
+
+CREATE TABLE user (
+                      user_id INT AUTO_INCREMENT PRIMARY KEY,
+                      name VARCHAR(100) NOT NULL,
+                      email VARCHAR(100) NOT NULL UNIQUE,
+                      password_hash VARCHAR(400) NOT NULL,
+                      role ENUM ('USER', 'ORGANIZER', 'ADMIN') DEFAULT ('USER'),
+                      last_logged_in DATE,
+                      date_asked_for_removal DATE,
+                      profile_visibility ENUM ('PRIVATE', 'PUBLIC')
+);
+
+CREATE TABLE event (
+                       event_id INT AUTO_INCREMENT PRIMARY KEY,
+                       owner_id INT,
+                       FOREIGN KEY (owner_id) REFERENCES user(user_id) ON DELETE CASCADE,
+                       max_slots INT NOT NULL,
+                       available_slots INT NOT NULL,
+                       location VARCHAR(200) NOT NULL,
+                       start_time TIME NOT NULL,
+                       date DATE NOT NULL
+);
+
+CREATE TABLE user_participate_event (
+                                        user_id INT,
+                                        event_id INT,
+                                        PRIMARY KEY (user_id, event_id),
+                                        FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+                                        FOREIGN KEY (event_id) REFERENCES event(event_id) ON DELETE CASCADE
+);
+
+CREATE TABLE card_list (
+                           card_id INT AUTO_INCREMENT PRIMARY KEY,
+                           black_mana INT DEFAULT 0,
+                           blue_mana INT DEFAULT 0,
+                           green_mana INT DEFAULT 0,
+                           red_mana INT DEFAULT 0,
+                           white_mana INT DEFAULT 0,
+                           neutral_mana INT DEFAULT 0,
+                           name VARCHAR(100) NOT NULL,
+                           supertype ENUM ('BASIC', 'LEGENDARY') DEFAULT ('BASIC'),
+                           card_type ENUM ('ARTIFACT', 'CREATURE', 'ENCHANTMENT', 'INSTANT', 'KINDRED', 'LAND', 'SORCERY') NOT NULL,
+                           multi_type ENUM ('ARTIFACT', 'CREATURE', 'ENCHANTMENT', 'INSTANT', 'KINDRED', 'LAND', 'SORCERY'),
+                           sub_type VARCHAR(100),
+                           can_be_commander BOOLEAN DEFAULT false,
+                           picture VARCHAR(100) NOT NULL,
+                           set_name VARCHAR(200) NOT NULL,
+                           rule_text VARCHAR(300) NOT NULL,
+                           toughness INT DEFAULT 0,
+                           power INT DEFAULT 0,
+                           rarity ENUM ('COMMON', 'UNCOMMON', 'RARE', 'MYTHIC_RARE') NOT NULL
+);
+
+CREATE TABLE owned_card (
+                            owned_card_id INT AUTO_INCREMENT PRIMARY KEY,
+                            card_id INT,
+                            FOREIGN KEY (card_id) REFERENCES card_list(card_id) ON DELETE CASCADE,
+                            user_id INT,
+                            FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+                            card_condition ENUM ('MINT', 'NEAR_MINT', 'EXCELLENT', 'GOOD', 'LIGHT_PLAYED', 'PLAYED', 'POOR', 'UNSPECIFIED') DEFAULT ('UNSPECIFIED'),
+                            foil VARCHAR(100)
+);
+
+CREATE TABLE deck (
+                      deck_id INT AUTO_INCREMENT PRIMARY KEY,
+                      user_id INT,
+                      FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+                      card_amount INT NOT NULL,
+                      format ENUM ('COMMANDER', 'STANDARD') NOT NULL
+);
+
+CREATE TABLE deck_contain_card (
+                                   deck_contain_id INT AUTO_INCREMENT PRIMARY KEY,
+                                   deck_id INT,
+                                   FOREIGN KEY (deck_id) REFERENCES deck(deck_id) ON DELETE CASCADE,
+                                   card_id INT,
+                                   FOREIGN KEY (card_id) REFERENCES card_list(card_id) ON DELETE CASCADE,
+                                   is_commander BOOLEAN DEFAULT false
+);
